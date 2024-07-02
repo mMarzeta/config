1cff836a877f3b47bcbd92b3569dbc7d585dc1ab
Maciej Marzęta <23502918+mMarzeta@users.noreply.github.com>
2024-07-02 08:47:00 +0200

    Kickstart config for nvim. Includes all necessary things

--- nvim/lua/plugins.lua
+++
@@ -1,7 +0,0 @@
-return require('packer').startup(function(use)
-  -- Configurations will go here soon
- use 'wbthomason/packer.nvim'
- use 'williamboman/mason.nvim'   
- use 'williamboman/mason-lspconfig.nvim'
- use 'neovim/nvim-lspconfig'
-end)
