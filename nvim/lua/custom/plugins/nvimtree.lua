3724fcfcf695059005e2900dd9ee96ab53838451
Maciej M <94maciekm@gmail.com>
2024-10-18 18:45:52 +0200

    modularized config

---
+++ nvim/lua/custom/plugins/nvimtree.lua
@@ -0,0 +1,35 @@
+-- Plugin configuration for nvim-tree
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    config = function()
+      -- disable netrw at the very start
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      -- enable 24-bit colour
+      vim.opt.termguicolors = true
+
+      -- setup nvim-tree with options
+      require('nvim-tree').setup {
+        sort = {
+          sorter = 'case_sensitive',
+        },
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
+      }
+    end,
+    lazy = false,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
